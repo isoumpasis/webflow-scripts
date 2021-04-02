bfb6d65b695f005ac26dc7b5f169766353fbb1d1
Giannis Soumpasis <wweacountss@gmail.com>
2021-04-02 11:53:44 +0300

    z-idenx modal

--- map.js
+++ map.js
@@ -88,26 +88,142 @@ async function initMap() {
 				west: 8.56
 			}
 		},
+		// styles: [
+		// 	{
+		// 		featureType: 'water',
+		// 		stylers: [{ color: '#cae4ff' }]
+		// 	},
+		// 	{
+		// 		featureType: 'landscape',
+		// 		elementType: 'geometry',
+		// 		stylers: [{ color: '#F7F9F9' }]
+		// 	},
+
+		// 	{
+		// 		featureType: 'road.highway',
+		// 		elementType: 'geometry',
+		// 		stylers: [{ color: '#D0E9FF' }]
+		// 	},
+		// 	{
+		// 		featureType: 'road',
+		// 		elementType: 'labels',
+		// 		stylers: [{ saturation: -100 }]
+		// 	}
+		// ],
 		styles: [
 			{
-				featureType: 'water',
-				stylers: [{ color: '#cae4ff' }]
+				featureType: 'all',
+				elementType: 'geometry',
+				stylers: [
+					{
+						color: '#e8f1f6'
+					}
+				]
+			},
+			{
+				featureType: 'all',
+				elementType: 'labels.text.fill',
+				stylers: [
+					{
+						gamma: 0.01
+					},
+					{
+						lightness: 20
+					}
+				]
+			},
+			{
+				featureType: 'all',
+				elementType: 'labels.text.stroke',
+				stylers: [
+					{
+						saturation: -31
+					},
+					{
+						lightness: -33
+					},
+					{
+						weight: 2
+					},
+					{
+						gamma: 0.8
+					}
+				]
+			},
+			{
+				featureType: 'all',
+				elementType: 'labels.icon',
+				stylers: [
+					{
+						visibility: 'off'
+					}
+				]
 			},
 			{
 				featureType: 'landscape',
 				elementType: 'geometry',
-				stylers: [{ color: '#F7F9F9' }]
+				stylers: [
+					{
+						lightness: 30
+					},
+					{
+						saturation: 30
+					}
+				]
 			},
-
 			{
-				featureType: 'road.highway',
+				featureType: 'poi',
 				elementType: 'geometry',
-				stylers: [{ color: '#D0E9FF' }]
+				stylers: [
+					{
+						saturation: 20
+					}
+				]
+			},
+			{
+				featureType: 'poi.park',
+				elementType: 'geometry',
+				stylers: [
+					{
+						lightness: 20
+					},
+					{
+						saturation: -20
+					}
+				]
 			},
 			{
 				featureType: 'road',
-				elementType: 'labels',
-				stylers: [{ saturation: -100 }]
+				elementType: 'geometry',
+				stylers: [
+					{
+						lightness: 10
+					},
+					{
+						saturation: -30
+					}
+				]
+			},
+			{
+				featureType: 'road',
+				elementType: 'geometry.stroke',
+				stylers: [
+					{
+						saturation: 25
+					},
+					{
+						lightness: 25
+					}
+				]
+			},
+			{
+				featureType: 'water',
+				elementType: 'all',
+				stylers: [
+					{
+						lightness: -20
+					}
+				]
 			}
 		]
 	};
