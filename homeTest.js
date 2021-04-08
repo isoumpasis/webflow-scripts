239d8fbc72fbb7a9b98c249c8eac3f757678118f
Giannis Soumpasis <wweacountss@gmail.com>
2021-04-08 09:55:02 +0300

    kfj

--- homeTest.js
+++ homeTest.js
@@ -469,8 +469,8 @@ creditCardInstallments.addEventListener('change', e => creditCardInstallmentsOnC
 function creditCardInstallmentsOnChange(value) {
 	console.log({ suggestedSystemPrices });
 	let cost = creditCardPrice1.previousElementSibling.checked
-		? suggestedSystemPrices[0] || parseFloat(creditCardPrice1.textContent.replace(' €', '')).toFixed(2)
-		: suggestedSystemPrices[1] || parseFloat(creditCardPrice2.textContent.replace(' €', '')).toFixed(2);
+		? suggestedSystemPrices[0] || parseFloat(creditCardPrice1.textContent.replace(' €', ''))
+		: suggestedSystemPrices[1] || parseFloat(creditCardPrice2.textContent.replace(' €', ''));
 
 	let installments = +value;
 	console.log({ installments });
