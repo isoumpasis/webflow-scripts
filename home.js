cc1c5f7218588ee203bd9def632a5acb0759796a
Giannis Soumpasis <wweacountss@gmail.com>
2021-05-29 11:12:03 +0300

    webflow push

--- home.js
+++ home.js
@@ -161,7 +161,7 @@ document.addEventListener('DOMContentLoaded', () => {
 	initSelectedFuelListeners();
 	initEasyPay();
 
-	localStorage.clear();
+	//localStorage.clear();
 	//initStorage();
 });
 
@@ -897,7 +897,7 @@ function showResults(fetchedModelObj) {
 	}
 
 	const suggestedContainer = getActiveContainer();
-	userSelections.suggestions = { ...userSelections.suggestions, containerId: activeContainer.id };
+	userSelections.suggestions = { ...userSelections.suggestions, containerId: suggestedContainer.id };
 	saveUserSelections();
 	adjustSectionPaddings();
 
