05d1bee0e719c4845e642f21cdc0fbdf512ac0ff
Giannis Soumpasis <isoumpasis@inf.uth.gr>
2021-06-12 18:48:46 +0300

    webflow push

--- gogas.js
+++ gogas.js
@@ -7,185 +7,195 @@ const typeSelect = document.querySelector('#typeSelect');
 const litresSelect = document.querySelector('#litresSelect');
 const dimensionSelect = document.querySelector('#dimensionSelect');
 const locationSelect = document.querySelector('#locationSelect');
-locationSelect.style.display = 'none';
+
+document.addEventListener('DOMContentLoaded', () => {
+  litresSelect.disabled = true;
+  dimensionSelect.disabled = true;
+
+  locationSelect.style.display = 'none';
+});
 
 const suggestedContainers = document.querySelectorAll('.suggested-tank-container');
 
 let fetchedLitres, fetchedDimensions, foundTankObj, activeContainer;
 
 const typeContainerIdDict = {
-	ΕΣΩΤΕΡΙΚΗ: 'eswterikhContainer',
-	ΕΞΩΤΕΡΙΚΗ: 'ekswterikhContainer',
-	ΚΥΛΙΝΔΡΙΚΗ: 'kylindrikhContainer'
+  ΕΣΩΤΕΡΙΚΗ: 'eswterikhContainer',
+  ΕΞΩΤΕΡΙΚΗ: 'ekswterikhContainer',
+  ΚΥΛΙΝΔΡΙΚΗ: 'kylindrikhContainer'
 };
 
 function startLoadingSelect(select) {
-	select.classList.add('loading-select');
+  select.classList.add('loading-select');
 }
 function endLoadingSelect(select) {
-	select.classList.remove('loading-select');
+  select.classList.remove('loading-select');
 }
 
 typeSelect.addEventListener('change', function () {
-	console.log('type changed', this.value);
+  console.log('type changed', this.value);
 
-	litresSelect.disabled = true;
-	dimensionSelect.disabled = true;
-	litresSelect.innerHTML = '<option value="">Λίτρα</option>';
-	dimensionSelect.innerHTML = '<option value="">Διαστάσεις</option>';
-	suggestedContainers.forEach(container => {
-		container.style.display = 'none';
-	});
+  litresSelect.disabled = true;
+  dimensionSelect.disabled = true;
+  litresSelect.innerHTML = '<option value="">Λίτρα</option>';
+  dimensionSelect.innerHTML = '<option value="">Διαστάσεις</option>';
+  suggestedContainers.forEach(container => {
+    container.style.display = 'none';
+  });
 
-	if (!this.value) return;
+  if (!this.value) return;
 
-	litresSelect.disabled = false;
-	litresSelect.innerHTML = '';
-	startLoadingSelect(litresSelect);
+  litresSelect.disabled = false;
+  litresSelect.innerHTML = '';
+  startLoadingSelect(litresSelect);
 
-	let status;
-	fetch(baseUrl + urlLitres, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ type: this.value })
-	})
-		.then(response => {
-			status = response.status;
-			return response.json();
-		})
-		.then(data => {
-			if (status !== 200) {
-				endLoadingSelect(litresSelect);
-				litresSelect.innerHTML = `<option value="">Προσπαθήστε ξανά ${data.msg}</option>`;
-				return;
-			}
-			console.log('Litres Fetch:', data);
-			fetchedLitres = data;
+  let status;
+  fetch(baseUrl + urlLitres, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ type: this.value })
+  })
+    .then(response => {
+      status = response.status;
+      return response.json();
+    })
+    .then(data => {
+      if (status !== 200) {
+        endLoadingSelect(litresSelect);
+        litresSelect.innerHTML = `<option value="">Προσπαθήστε ξανά ${data.msg}</option>`;
+        return;
+      }
+      console.log('Litres Fetch:', data);
+      fetchedLitres = data;
 
-			populateLitresSelect(fetchedLitres);
-			endLoadingSelect(litresSelect);
-		})
-		.catch(error => {
-			endLoadingSelect(litresSelect);
-			litresSelect.innerHTML = '<option value="">Προσπαθήστε ξανά</option>';
-			console.error('Error Fetch:', error);
-		});
+      populateLitresSelect(fetchedLitres);
+      endLoadingSelect(litresSelect);
+    })
+    .catch(error => {
+      endLoadingSelect(litresSelect);
+      litresSelect.innerHTML = '<option value="">Προσπαθήστε ξανά</option>';
+      console.error('Error Fetch:', error);
+    });
 });
 
 function populateLitresSelect(fetchedLitres) {
-	let litresOptionsArray = ['<option value="">Επιλέξτε Λίτρα</option>'];
+  let litresOptionsArray = ['<option value="">Επιλέξτε Λίτρα</option>'];
 
-	const allDimensionsTypeLabel =
-		typeSelect.value !== 'unknown' ? ` για ${typeSelect.options[typeSelect.selectedIndex].textContent.toLowerCase()}` : '';
-	litresOptionsArray.push(`<option value="allDimensions">Όλες οι διαστάσεις${allDimensionsTypeLabel}</option>`);
+  const allDimensionsTypeLabel =
+    typeSelect.value !== 'unknown'
+      ? ` για ${typeSelect.options[typeSelect.selectedIndex].textContent.toLowerCase()}`
+      : '';
+  litresOptionsArray.push(
+    `<option value="allDimensions">Όλες οι διαστάσεις${allDimensionsTypeLabel}</option>`
+  );
 
-	fetchedLitres.forEach(litre => {
-		litresOptionsArray.push(`<option value="${litre}">${litre} LT</option>`);
-	});
+  fetchedLitres.forEach(litre => {
+    litresOptionsArray.push(`<option value="${litre}">${litre} LT</option>`);
+  });
 
-	litresSelect.innerHTML = litresOptionsArray.join('');
-	litresSelect.disabled = false;
-	litresSelect.focus();
-	//One option -> auto populate
-	if (litresOptionsArray.length === 2) {
-		litresSelect.selectedIndex = 1;
-		litresOnChange(litresSelect.value);
-		return;
-	}
+  litresSelect.innerHTML = litresOptionsArray.join('');
+  litresSelect.disabled = false;
+  litresSelect.focus();
+  //One option -> auto populate
+  if (litresOptionsArray.length === 2) {
+    litresSelect.selectedIndex = 1;
+    litresOnChange(litresSelect.value);
+    return;
+  }
 }
 
 litresSelect.addEventListener('change', e => litresOnChange(e.target.value));
 function litresOnChange(value) {
-	console.log('liters changed', value);
-	dimensionSelect.disabled = true;
-	dimensionSelect.innerHTML = '<option>Διαστάσεις</option>';
-	suggestedContainers.forEach(container => {
-		container.style.display = 'none';
-	});
+  console.log('liters changed', value);
+  dimensionSelect.disabled = true;
+  dimensionSelect.innerHTML = '<option>Διαστάσεις</option>';
+  suggestedContainers.forEach(container => {
+    container.style.display = 'none';
+  });
 
-	if (!value) return;
+  if (!value) return;
 
-	dimensionSelect.disabled = false;
-	dimensionSelect.innerHTML = '';
-	startLoadingSelect(dimensionSelect);
+  dimensionSelect.disabled = false;
+  dimensionSelect.innerHTML = '';
+  startLoadingSelect(dimensionSelect);
 
-	let status;
-	fetch(baseUrl + urlDimensions, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ type: typeSelect.value, litres: value })
-	})
-		.then(response => {
-			status = response.status;
-			return response.json();
-		})
-		.then(data => {
-			if (status !== 200) {
-				endLoadingSelect(dimensionSelect);
-				litresSelect.innerHTML = `<option value="">Προσπαθήστε ξανά ${data.msg}</option>`;
-				return;
-			}
-			console.log('Dimensions Fetch:', data);
-			fetchedDimensions = data;
-			populateDimensionSelect(fetchedDimensions);
-			endLoadingSelect(dimensionSelect);
-		})
-		.catch(error => {
-			endLoadingSelect(dimensionSelect);
-			litresSelect.innerHTML = '<option value="">Προσπαθήστε ξανά</option>';
-			console.error('Error Fetch:', error);
-		});
+  let status;
+  fetch(baseUrl + urlDimensions, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ type: typeSelect.value, litres: value })
+  })
+    .then(response => {
+      status = response.status;
+      return response.json();
+    })
+    .then(data => {
+      if (status !== 200) {
+        endLoadingSelect(dimensionSelect);
+        litresSelect.innerHTML = `<option value="">Προσπαθήστε ξανά ${data.msg}</option>`;
+        return;
+      }
+      console.log('Dimensions Fetch:', data);
+      fetchedDimensions = data;
+      populateDimensionSelect(fetchedDimensions);
+      endLoadingSelect(dimensionSelect);
+    })
+    .catch(error => {
+      endLoadingSelect(dimensionSelect);
+      litresSelect.innerHTML = '<option value="">Προσπαθήστε ξανά</option>';
+      console.error('Error Fetch:', error);
+    });
 }
 
 function populateDimensionSelect(fetchedDimensions) {
-	let dimensionOptionsArray = ['<option value="">Επιλέξτε Διαστάσεις</option>'];
-	fetchedDimensions.forEach(dimension => {
-		const typeLabel = typeSelect.value === 'unknown' ? ` ${dimension.type} ` : '';
-		const dimensionLabel = `${dimension.diameter}/${dimension.length}${typeLabel} - ${dimension.litres} LT`;
-		dimensionOptionsArray.push(`<option value="${dimension.id}">${dimensionLabel}</option>`);
-	});
+  let dimensionOptionsArray = ['<option value="">Επιλέξτε Διαστάσεις</option>'];
+  fetchedDimensions.forEach(dimension => {
+    const typeLabel = typeSelect.value === 'unknown' ? ` ${dimension.type} ` : '';
+    const dimensionLabel = `${dimension.diameter}/${dimension.length}${typeLabel} - ${dimension.litres} LT`;
+    dimensionOptionsArray.push(`<option value="${dimension.id}">${dimensionLabel}</option>`);
+  });
 
-	dimensionSelect.innerHTML = dimensionOptionsArray.join('');
-	dimensionSelect.disabled = false;
-	dimensionSelect.focus();
+  dimensionSelect.innerHTML = dimensionOptionsArray.join('');
+  dimensionSelect.disabled = false;
+  dimensionSelect.focus();
 
-	if (dimensionOptionsArray.length === 2) {
-		dimensionSelect.selectedIndex = 1;
-		dimensionOnChange(dimensionSelect.value);
-		return;
-	}
+  if (dimensionOptionsArray.length === 2) {
+    dimensionSelect.selectedIndex = 1;
+    dimensionOnChange(dimensionSelect.value);
+    return;
+  }
 }
 
 dimensionSelect.addEventListener('change', e => dimensionOnChange(e.target.value));
 
 function dimensionOnChange(value) {
-	console.log('dimension changed', value);
+  console.log('dimension changed', value);
 
-	suggestedContainers.forEach(container => {
-		container.style.display = 'none';
-	});
-	if (value !== 0 && !value) return;
+  suggestedContainers.forEach(container => {
+    container.style.display = 'none';
+  });
+  if (value !== 0 && !value) return;
 
-	showResults();
+  showResults();
 }
 
 function showResults() {
-	foundTankObj = fetchedDimensions.find(dim => +dimensionSelect.value == dim.id);
-	console.log({ foundTankObj });
+  foundTankObj = fetchedDimensions.find(dim => +dimensionSelect.value == dim.id);
+  console.log({ foundTankObj });
 
-	activeContainer = document.getElementById(typeContainerIdDict[foundTankObj.type]);
-	renderResultContainer(activeContainer);
+  activeContainer = document.getElementById(typeContainerIdDict[foundTankObj.type]);
+  renderResultContainer(activeContainer);
 }
 
 function renderResultContainer(container) {
-	container.querySelector('.litres-result').textContent = foundTankObj.litres + ' LT';
-	container.querySelector('.diameter-result').textContent = foundTankObj.diameter / 10;
-	container.querySelector('.length-result').textContent = foundTankObj.length / 10;
-	container.querySelector('.price-result').textContent = foundTankObj.price + '€';
+  container.querySelector('.litres-result').textContent = foundTankObj.litres + ' LT';
+  container.querySelector('.diameter-result').textContent = foundTankObj.diameter / 10;
+  container.querySelector('.length-result').textContent = foundTankObj.length / 10;
+  container.querySelector('.price-result').textContent = foundTankObj.price + '€';
 
-	container.style.display = 'grid';
+  container.style.display = 'grid';
 }
