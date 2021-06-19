393c5578211223485933f0fc47d6543839314f2f
Giannis Soumpasis <wweacountss@gmail.com>
2021-06-19 10:45:42 +0300

    webflow push

--- gogas.js
+++ gogas.js
@@ -394,7 +394,7 @@ function populateClosestsPins(userLatLng) {
       geometry: userLatLng,
       lovatoServices: ['gogasTanks'],
       kmLimit: 150,
-      pinsLimit: 450
+      pinsLimit: 20
     })
   })
     .then(response => {
