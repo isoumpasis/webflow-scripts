7caebf5507f6849e25d7311d790c89694490b311
Giannis Soumpasis <wweacountss@gmail.com>
2021-06-24 09:56:19 +0300

    webflow push

--- map.js
+++ map.js
@@ -1210,7 +1210,7 @@ async function urlParamsConfig() {
     if (geometry.some(el => isNaN(el)) || geometry.length !== 2) return;
 
     const foundMarker = markers.find(
-      m => m.props.geometry.lat === geometry[0] && m.props.geometry.lng === geometry[1]
+      m => m.geometry.lat === geometry[0] && m.geometry.lng === geometry[1]
     );
 
     if (!foundMarker) return console.log(`marker on ${geometry} not found`);
