d1fd903890771d1f4c61e46729aefe33141a1ec0
Giannis Soumpasis <wweacountss@gmail.com>
2021-06-26 13:27:19 +0300

    webflow push

--- home.js
+++ home.js
@@ -2026,12 +2026,14 @@ function updateBasketSection(sections) {
         el => (el.style.display = 'none')
       );
     } else if (userSelections.easyPay.method === 'Μετρητά') {
+      console.log('methrta!!!');
       document.querySelector('.easy-pay-final-cost-basket').textContent =
         userSelections.easyPay.system.priceWithVAT;
       document.querySelector('.easy-pay-yearly-gain').textContent = userSelections.calculator
         .perMonthCheckbox
         ? (+userSelections.calculator.gain.replace('€', '') * 12).toFixed(2) + '€'
         : userSelections.calculator.gain;
+      console.log('methrta!!!', JSON.stringify(userSelections.calculator));
       [...document.querySelectorAll('.not-needed-row-metrhta-basket')].map(
         el => (el.style.display = 'none')
       );
