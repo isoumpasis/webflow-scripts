907b54a09a55df2c395a6f1b4fa22aff8af45f5e
Giannis Soumpasis <wweacountss@gmail.com>
2021-07-17 10:13:44 +0300

    webflow push

--- home.js
+++ home.js
@@ -2479,9 +2479,7 @@ function validateUserForm() {
   return true;
 }
 function isEmail(email) {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  return re.test(String(email).toLowerCase());
+  return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(String(email).toLowerCase());
 }
 
 function handleInvalidDownload() {
