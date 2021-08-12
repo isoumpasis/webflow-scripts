5f4af215b9aad81aaa61a1cc2effd00fe7e2663e
Giannis Soumpasis <wweacountss@gmail.com>
2021-08-12 11:31:49 +0300

    webflow push

--- lovatoApp.js
+++ lovatoApp.js
@@ -100,6 +100,12 @@ function validateContactForm() {
   return { valid: true };
 }
 
+function isEmail(email) {
+  const re =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
 function hasUserInfo() {
   const ret = getUserInfo();
 
