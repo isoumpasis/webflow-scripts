3f85e2955f9125f71e17e9bf3b91d2cc0c8fe318
isoumpasis <wweacountss@gmail.com>
2021-08-28 00:23:31 +0300

    webflow push

--- gogas.js
+++ gogas.js
@@ -965,7 +965,7 @@ const minutesCountdown = document.querySelector('#minutes');
 const secondsCountdown = document.querySelector('#seconds');
 
 function initLotteryCountdown() {
-  let baseDate = new Date(2021, 7, 28, 0, 23);
+  let baseDate = new Date(2021, 7, 31, 0, 23);
   let nextLotteryDate = baseDate;
   let remainingMilliseconds = nextLotteryDate - new Date();
   calculateTime(remainingMilliseconds);
